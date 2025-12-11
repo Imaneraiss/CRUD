8cb9923ab4ced46d7140b7aefce07bbc386e8811
Imane Raiss <imane.raiss100@gmail.com>
2025-12-11 15:36:51 +0100

    getTotal-createPro-showData-CleanData-deletePro

---
+++ script.js
@@ -0,0 +1,116 @@
+let price = document.getElementById('price');
+let taxes = document.getElementById('taxes');
+let ads = document.getElementById('ads');
+let discount = document.getElementById('discount');
+let total = document.getElementById('total');
+let count = document.getElementById('count');
+let category = document.getElementById('category');
+let submit =document.getElementById('submit');
+let update = document.getElementById('update');
+
+
+
+
+// get total 
+function getTotal () {
+    if (price.value != ''){
+        let result = (+price.value + +taxes.value + +ads.value - +discount.value);
+        total.innerHTML = result;
+        total.style.background='#040';
+    }else {
+        total.innerHTML = '';
+        total.style.background=' rgba(207, 1, 1, 0.596)';
+    }
+}
+
+let dataPro;
+if (localStorage.product != null){
+    dataPro = JSON.parse(localStorage.product);
+}else{
+    dataPro = [];
+}
+
+// create product 
+submit.addEventListener('click',function(){
+
+    if(title.value != '' & total.innerHTML != '' ){
+        let newPro= {
+            title:title.value,
+            price:price.value,
+            taxes:taxes.value,
+            ads:ads.value,
+            discount:discount.value ,
+            total: total.innerHTML,
+            count:count.value ,
+            category:category.value,
+        }
+        dataPro.push(newPro);
+        localStorage.setItem('product',JSON.stringify(dataPro));
+        console.log(dataPro);
+        clearData()
+        showData()
+    }else{
+        window.alert('Remplir tous les champs requis!');
+    }
+})
+
+
+
+
+// clear inputs
+function clearData(){
+    title.value = '';
+    price.value = '';
+    taxes.value = '';
+    ads.value = '';
+    discount.value = '';
+    total.innerHTML = '';
+    count.value = '';
+    category.value = '';
+    total.style.background=' rgba(207, 1, 1, 0.596)';
+}
+
+
+//read 
+function showData(){
+
+    let tbody ='';
+    for (let i = 0; i< dataPro.length ;i++){
+        tbody +=`
+            <tr>
+                <td>${i+1}</td>
+                <td>${dataPro[i].title}</td>
+                <td>${dataPro[i].price}</td>
+                <td>${dataPro[i].taxes}</td>
+                <td>${dataPro[i].discount}</td>
+                <td>${dataPro[i].total}</td>
+                <td>${dataPro[i].category}</td>
+                <td><button onclick="updateData()" id="update">update</button></td>
+                <td><button onclick="deleteData(${i})" id="delete">delete</button></td>
+            
+            </tr>
+        `;
+    }
+    console.log('done');
+    document.getElementById('tbody').innerHTML = tbody;
+}
+window.onload = function(){
+    showData();
+}
+
+
+//delete
+
+function deleteData(i){
+    dataPro.splice(i,1); // delete from i 1 elem 
+    localStorage.product = JSON.stringify(dataPro);
+    showData();
+}
+
+
+// search
+// create count product
+// update 
+// delete
+// count 
+// clean data
